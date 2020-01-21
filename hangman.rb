b857a8bbf26189ce3872fefbcedb163d59dc17bb
Goldpm <lowedavidadam@gmail.com>
2020-01-21 14:44:45 -0500

    added check to see if user guessed an actual letter

--- hangman.rb
+++ hangman.rb
@@ -6,12 +6,19 @@ class Hangman
     def words
         ["variable", "class", "def", "puts", "unless"]
     end
+
+    def check_answer(answer)
+        unless @letter.include? answer
+            puts "You have to guess a letter!"
+        end
+        
+    end
     
     def start
         puts "This is ruby hangman, guess a letter of a ruby term! your word is #{@word.length} letters long"
         puts "_ " * @word.length
         input = gets.chomp
-        puts "You put #{input}, and the word is #{@word}"
+        check_answer(input)
     end
 end
 
