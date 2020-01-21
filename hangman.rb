844f63ad409b5a6bce8b6c75b479264fbeb87571
Goldpm <lowedavidadam@gmail.com>
2020-01-21 14:05:15 -0500

    now gets word from array

--- hangman.rb
+++ hangman.rb
@@ -1,8 +1,15 @@
 class Hangman
+    def initialize
+        @word = words.sample
+    end
+    def words
+        ["Base"]
+    end
+    
     def check
         puts "Input anything!"
         input = gets.chomp
-        puts "You put #{input}"
+        puts "You put #{input}, and the word is #{@word}"
     end
 end
 
