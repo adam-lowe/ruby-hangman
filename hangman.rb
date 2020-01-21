2e81bf5de05f57feebdb669d1c9027cb44903d02
Goldpm <lowedavidadam@gmail.com>
2020-01-21 14:19:11 -0500

    changed check def to start to avoid confusion

--- hangman.rb
+++ hangman.rb
@@ -6,7 +6,7 @@ class Hangman
         ["variable", "class", "def", "puts", "unless"]
     end
     
-    def check
+    def start
         puts "This is ruby hangman, guess a ruby term! your word is #{@word.length} letters long"
         input = gets.chomp
         puts "You put #{input}, and the word is #{@word}"
@@ -15,4 +15,4 @@ end
 
 game = Hangman.new
 
-game.check
+game.start
