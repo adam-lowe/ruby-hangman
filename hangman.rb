e309cd6fda3951808a219419066fa5dc23386485
Goldpm <lowedavidadam@gmail.com>
2020-01-21 14:08:52 -0500

    added some more words and had it read the chosen word length

--- hangman.rb
+++ hangman.rb
@@ -3,11 +3,11 @@ class Hangman
         @word = words.sample
     end
     def words
-        ["Base"]
+        ["variable", "class", "def"]
     end
     
     def check
-        puts "Input anything!"
+        puts "This is ruby hangman, guess a ruby term! your word is #{@word.length} letters long"
         input = gets.chomp
         puts "You put #{input}, and the word is #{@word}"
     end
