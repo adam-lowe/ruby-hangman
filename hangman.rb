78778ac6ae57c650c7275308c1fc886a7134ac01
Goldpm <lowedavidadam@gmail.com>
2020-01-23 17:42:16 -0500

    The game now has a proper end!

--- hangman.rb
+++ hangman.rb
@@ -2,7 +2,7 @@ class Hangman
     def initialize
         @word = words.sample
         @letter = ('a'..'z').to_a
-        @game_finished = false
+        @game_status = [0, @word.length]
         @blank = "_ " * @word.length
     end
     def words
@@ -17,6 +17,7 @@ class Hangman
             puts "That's a correct letter!"
             matches = @word.split("").each_index.select{|i| @word[i] == answer}
             matches.each { |i| @blank[i*2] = answer}
+            @game_status[0] += 1
             @letter.delete(answer)
         else
             puts "Incorrect letter"
@@ -28,7 +29,7 @@ class Hangman
         puts @blank
         input = gets.chomp
         check_answer(input)
-        if @game_finished == true
+        if @game_status[0] == @game_status[1] 
             game_end
         else
             guess
@@ -40,6 +41,11 @@ class Hangman
         puts "This is ruby hangman, guess a letter of a ruby term! your word is #{@word.length} letters long"
         guess
     end
+
+    def game_end
+        p "Congrats! You got the whole word! #{@word}! Pat youself on the back!"
+    end
+    
 end
 
 game = Hangman.new
